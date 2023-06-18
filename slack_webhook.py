4549d2d3742e81731f7a489868552f10de7ed40a
danglephd <danglephd@gmail.com>
2023-06-18 20:10:16 +0700

    Add HOOK

--- slack_webhook.py
+++ slack_webhook.py
@@ -2,14 +2,17 @@ import os
 from slackeventsapi import SlackEventAdapter
 from slack_sdk import WebClient
 from slack_sdk.webhook import WebhookClient
+from dotenv import load_dotenv
 
 try:
+    load_dotenv()
     # SLACK_VERIFICATION_TOKEN = os.environ["SLACK_VERIFICATION_TOKEN"]
 
     # # Create a SlackClient for your bot to use for Web API requests
     SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
     # SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
     CHANNEL_ID = os.environ["CHANNEL_ID"]
+    SLACK_HOOK = os.environ["SLACK_HOOK"]
     slack_client = WebClient(SLACK_BOT_TOKEN)
 except KeyError:
     print("Environment variable does not exist", KeyError)
@@ -21,10 +24,10 @@ slack = WebhookClient(
 slack.send(text="Hello, world.")
 
 
-def send_survey(user, channel, text):
+def send_survey(user, text, channel = CHANNEL_ID):
     # More info: https://api.slack.com/docs/message-menus
     # Send an in-channel reply to the user
-    print('>>>send_survey ', user, channel, text)
+    print('>>>send_survey ', user, text, channel)
     slack_client.api_call(
         api_method="chat.postMessage", json={"channel": channel, "text": text}
     )

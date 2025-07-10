57943c4644e0f375b83e9e4af2d6d762f31dddd8
danglephd@gmail.com <danglephd@gmail.com>
2025-07-10 16:03:02 +0700

    check no issue first

--- src/modules/gitlab/find_issue_page.py
+++ src/modules/gitlab/find_issue_page.py
@@ -4,27 +4,51 @@ from selenium.common.exceptions import TimeoutException
 
 
 def get_gitlab_issue_info(driver, wait, project, new_issue_url, issue_link_list):
+    """
+    Thu thập thông tin các issue từ trang GitLab.
+
+    Hàm này sẽ:
+    - Đợi trang load xong container chứa danh sách issue.
+    - Kiểm tra nếu xuất hiện thông báo "Sorry, your filter produced no results" thì kết thúc và không thu thập issue nào.
+    - Nếu không có thông báo trên, sẽ lấy danh sách các issue hiện có và thêm vào issue_link_list.
+
+    Tham số:
+        driver: Đối tượng Selenium WebDriver.
+        wait: Đối tượng WebDriverWait để chờ các phần tử xuất hiện.
+        project: Tên dự án hiện tại.
+        new_issue_url: Đường dẫn tạo issue mới.
+        issue_link_list: Danh sách để lưu thông tin các issue thu thập được.
+
+    Trả về:
+        None. Kết quả được lưu vào issue_link_list.
+    """
     print(">Get Gitlab Issue Information")
+    # Wait for the page to be fully loaded by checking for the presence of the main container
+    wait.until(expected_conditions.presence_of_element_located(
+      (By.XPATH, "//div[contains(@class, 'issuable-list-container')]")
+    ))
     try:
-      elem = wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//ul[@class='content-list issuable-list issues-list']/li")))
-      # print(">>>>elem: ", elem)
-      elems = driver.find_elements(By.XPATH, "//div[@class='issuable-list-container']/ul/li")
-    #   print(">>>>elems: ", elems)
-      for li in elems:
-        # print('>>>li', li)
-        att = li.get_attribute("data-qa-issuable-title")
-        # print('>>>att li', att)
-        tag_a = li.find_element(By.XPATH, ".//a[@class='gl-link issue-title-text']")
-        # print(">>>>tag_a: ", tag_a)
-        issue_url = tag_a.get_attribute("href")
-        issue_text = tag_a.get_attribute("text")
-        # print(">>>>url: ", issue_url)
-        # print(">>>>text: ", issue_text)
-        iss_number = issue_url[issue_url.rfind("/") + 1:]
-        # print(">>>>iss Number: ", iss_number)
-        issue_link_list.append([iss_number, issue_url, project, new_issue_url, issue_text])
-      # print(">>>>issue_link_list: ", issue_link_list)
+      # Select and click "New task" option
+      no_result_label = wait.until(expected_conditions.presence_of_element_located(
+        (By.XPATH, "//h1[contains(text(),'Sorry, your filter produced no results')]")
+      ))
+
+      if no_result_label:
+        print(">>>No new issue")
+        return
+      
+    except TimeoutException as ex:
+      print(">>>New issue exists")
+      
+    try:
+        elems = driver.find_elements(By.XPATH, "//div[@class='issuable-list-container']/ul/li")
+        for li in elems:
+          att = li.get_attribute("data-qa-issuable-title")
+          tag_a = li.find_element(By.XPATH, ".//a[@class='gl-link issue-title-text']")
+          issue_url = tag_a.get_attribute("href")
+          issue_text = tag_a.get_attribute("text")
+          iss_number = issue_url[issue_url.rfind("/") + 1:]
+          issue_link_list.append([iss_number, issue_url, project, new_issue_url, issue_text])
     except TimeoutException as ex:
       print("TimeoutException has been thrown. " + str(ex.msg))
-      # send_survey(user="get", text=""":interrobang::interrobang::interrobang: *Error* on *Get Gitlab Issue Information.* :broken_heart::broken_heart::broken_heart:\nPlease get help from your Administrator.""")
     print("project issue_link_list. len ", len(issue_link_list))
